ac837655ab4841cdf58778fde529bfe68797bb2f
Mostafa Mossad <mostafa-mosad1@example.com>
2025-06-29 21:43:10 +0300

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,93 @@
 const btnMenu = document.querySelector("#btn-menu");
 const showMenu = document.querySelector("#show-menu");
 const closeMenu = document.querySelector("#close-menu");
+const navLinks = document.querySelector(".all-section-title");
+const productsCard = document.querySelector(".products-card");
+
+//  Header
+
+const allSection = [
+  {
+    title: "الرئــيسية",
+    arrow: false,
+    activ:true
+  },
+  {
+    title: "المدونة",
+    arrow: false,
+    activ:false,
+  },
+
+  {
+    title: "تخفيضات",
+    arrow: true,
+    activ:false
+  },
+  {
+    title: "العقود",
+    arrow: true,
+    activ:false
+  },
+  {
+    title: "الخواتم",
+    arrow: true,
+    activ:false
+  },
+  {
+    title: "الأساور",
+    arrow: true,
+    activ:false
+  },
+  {
+    title: "السلاسل",
+    arrow: true,
+    activ:false
+  },
+  {
+    title: "الحلقان",
+    arrow: true,
+    activ:false
+  },
+  {
+    title: "خواتم الخطوبة",
+    arrow: true,
+    activ:false
+  },
+  {
+    title: "اكسسوارات الشعر",
+    arrow: true,
+    activ:false
+  },
+  {
+    title: "الساعات",
+    arrow: true,
+    activ:false
+  },
+];
+
+let containerAllSection = "";
+
+allSection.forEach((sec, index) => {
+  containerAllSection += `
+          <li>
+            <a
+              class="w-[200px] h-[22px] font-harmattan font-semibold text-[18px] leading-[22px] tracking-normal text-right"
+              href="#"
+            >
+              <div class="flex justify-between px-6 py-3 items-baseline">
+                ${sec.title}
+                ${sec.arrow?`<img src="./images/icons/down-arrow 1.svg" class="size-4" alt="" />`:""}
+              </div>
+            </a>
+          </li>
+          <li>
+            <hr />
+          </li>
+          `;
+});
+
+
+navLinks.innerHTML = containerAllSection;
 
 btnMenu.addEventListener(`click`, function (e) {
   e.preventDefault();
@@ -10,109 +97,145 @@ closeMenu.addEventListener(`click`, function (e) {
   e.preventDefault();
   showMenu.classList.add("translate-x-full");
 });
-// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// sec-3
+
+const latestProductsData = [
+  {
+    img1:"./images/latest-products/new-product1.jpg",
+    img2:"./images/latest-products/new-product3.jpg",
+    title:"أكسسوارات شعر",
+    des:"اكسسوار باللون الدهبي",
+    price:"200 ر.س",
+    oldPrice:"300 ر.س"
+  },
+  {
+    img1:"./images/latest-products/new-product2.jpg",
+    img2:"./images/latest-products/new-product4.jpg",
+    title:" العقود",
+    des:"عقد أنيق",
+    price:"500 ر.س",
+    oldPrice:"650 ر.س"
+  },
+  {
+    img1:"./images/latest-products/new-product3.jpg",
+    img2:"./images/latest-products/new-product1.jpg",
+    title:"أساور إنسيال",
+    des:"أسورة جنزير",
+    price:"700 ر.س",
+    oldPrice:"800 ر.س"
+  },
+  {
+    img1:"./images/latest-products/new-product4.jpg",
+    img2:"./images/latest-products/new-product2.jpg",
+    title:"الخواتم",
+    des:"خاتم خطبة أنيق",
+    price:"300 ر.س",
+    oldPrice:"450 ر.س"
+  },
+]
 
 
+let latestProductsContainer =``;
 
+latestProductsData.forEach((product,index)=>{
+  latestProductsContainer += `
+    <div
+          class="w-[196px] md:w-[302px] group bg-mainColor bg-opacity-10 rounded-[16px] shadow-md p-2 flex flex-col space-y-2 text-right font-harmattan"
+        >
+          <div class="relative overflow-hidden">
+            <img
+              src="${product.img1}"
+              alt="الخواتم  "
+              class="rounded-[12px] sub-slider absolute inset-0 transition-opacity duration-500 group-hover:opacity-0"
+            />
+            <img
+              src="${product.img2}"
+              alt="الخواتم  "
+              class="rounded-[12px] sub-slider transition-opacity duration-500 opacity-0 group-hover:opacity-100"
+            />
+            <img
+              class="absolute bg-[#F3F3F3] p-2 rounded-full top-4 left-4"
+              src="./images/heart.svg"
+              alt="heart"
+            />
 
-window.addEventListener("DOMContentLoaded", () => {
-  const items = [
-    { title: "العقود", image: "./images/product-1.jpg" },
-    { title: "الساعات", image: "./images/product-2.jpg" },
-    { title: "الأطقم", image: "./images/product-3.jpg" },
-    { title: "الحلقان", image: "./images/product-1.jpg" },
-    { title: "الأساور", image: "./images/product-2.jpg" },
-  ];
+            <div
+              class="absolute flex justify-center items-end gap-4 p-2 -bottom-20 group-hover:bottom-4 transition-all duration-500 inset-x-0 mx-auto"
+            >
+              <img
+                class="bg-[#F3F3F3] p-2 rounded-full"
+                src="./images/heart.svg"
+                alt="heart"
+              />
+              <img
+                class="bg-[#F3F3F3] p-2 rounded-full"
+                src="./images/heart.svg"
+                alt="heart"
+              />
+            </div>
+          </div>
 
-  let currentIndex = 2;
-  const slider = document.getElementById("slider");
+          <div class="flex justify-between items-center">
+            <p
+              class="w-[250px] h-[30px] md:w-[200px] md:h-[36px] text-mainColor font-harmattan text-[12px] md:text-[20px] font-normal leading-[19px] md:leading-[25px] opacity-85"
+            >
+              ${product.title}
+            </p>
 
-  if (!slider) {
-    console.error("❌ عنصر #slider غير موجود");
-    return;
-  }
+            <div class="flex w-fit text-yellow-400 text-[16px] space-x-1">
+              <span>★</span><span>★</span><span>★</span><span>★</span
+              ><span>★</span>
+            </div>
+          </div>
 
-  function renderCards() {
-    slider.innerHTML = "";
+          <p
+            class="text-[#252525] text-[16px] md:text-[22px] leading-[18px] md:leading-[25px] font-medium font-harmattan"
+          >
+        ${product.des}
+          </p>
 
-    for (let offset = -2; offset <= 2; offset++) {
-      const index = (currentIndex + offset + items.length) % items.length;
-      const item = items[index];
-
-      const card = document.createElement("div");
-      card.className =
-          `card ${"card-"+index} animate-in rounded-xl flex-shrink-0 overflow-hidden bg-white/10 backdrop-blur-md text-center text-white shadow-lg`;
-
-      // responsive
-      const isMobile = window.innerWidth <= 640;
-      card.style.width = offset === 0 ? (isMobile ? "220px" : "300px") : (isMobile ? "180px" : "270px");
-      card.style.height = offset === 0 ? (isMobile ? "340px" : "510px") : (isMobile ? "300px" : "500px");
-
-      card.style.transition =
-        "transform 0.9s cubic-bezier(0.8, 0, 0.6, 1), width 0.9s, height 0.9s";
-
-      if (offset === 0) card.classList.add("active");
-      else if (offset < 0) card.classList.add("tilt-left");
-      else card.classList.add("tilt-right");
-
-      card.innerHTML = `
-        <div class="relative w-full h-full flex flex-col justify-end">
-          <img src="${item.image}" alt="${item.title}" class="absolute inset-0 w-full h-full object-cover rounded-xl z-0" />
-          <div class="relative z-10 bg-gradient-to-t from-black/70 via-black/30 to-transparent px-4 py-3 rounded-b-xl flex flex-col items-center">
-            <div class="text-lg font-bold mb-2">${item.title}</div>
-            <a href="#" class="text-white underline text-base">تسوق الآن</a>
+          <div class="flex items-center gap-2 w-[112px] md:w-[160px] h-[30px]">
+            <span
+              class="text-[#B70404] w-[58px] font-harmattan text-[16px] md:text-[18px]"
+              >${product.price}</span
+            >
+            <span
+              class="text-[#25252580] opacity-50 w-[49px] line-through text-[14px]"
+              >${product.oldPrice}</span
+            >
           </div>
         </div>
-      `;
-
-      slider.appendChild(card);
-    }
-  }
-
-  function next() {
-    currentIndex = (currentIndex + 1) % items.length;
-    renderCards();
-  }
-
-  function prev() {
-    currentIndex = (currentIndex - 1 + items.length) % items.length;
-    renderCards();
-  }
-
-  window.next = next;
-  window.prev = prev;
-
-  renderCards();
+  `;
 });
 
+productsCard.innerHTML = latestProductsContainer;
 
-
-
-// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// sec-5
 
 document.addEventListener("DOMContentLoaded", () => {
   const mainSubSlider = document.querySelector(".main-sub-slider");
 
   const SliderData = [
     {
-      main: "./images/product-1.jpg",
-      sub: "./images/sub-slider1.jpg",
+      main: "./images/sec2-slider/product-1.jpg",
+      sub: "./images/sec2-slider/sub-slider1.jpg",
       name: "السلاسل",
       des: "سلسة حمامة ",
       price: "500 ر.س",
       oldPrice: "650 ر.س",
     },
     {
-      main: "./images/product-2.jpg",
-      sub: "./images/sub-slider2.jpg",
+      main: "./images/sec2-slider/product-2.jpg",
+      sub: "./images/sec2-slider/sub-slider2.jpg",
       name: "السلاسل",
       des: "سلسة قلب حجر  ",
       price: "500 ر.س",
       oldPrice: "540 ر.س",
     },
     {
-      main: "./images/product-3.jpg",
-      sub: "./images/sub-slider3.svg",
+      main: "./images/sec2-slider/product-3.jpg",
+      sub: "./images/sec2-slider/sub-slider3.svg",
       name: "الحلقان",
       des: "حلق ذهبي دائري",
       price: "500 ر.س",
@@ -123,7 +246,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let SliderIndexData = 0;
 
   function changeSliderData(index) {
-    mainSubSlider.innerHTML = `<div class="flex  py-[50px] flex-col items-center md:flex-row gap-6">
+    mainSubSlider.innerHTML = `<div class="flex relative z-[999] py-[50px] flex-col items-center md:flex-row gap-6">
           <div class="flex flex-col items-center">
             <h2
               class="pt-[32px] w-[157px] h-[48px] md:w-[235px] md:h-[48px] font-harmattan font-normal text-[32px] md:text-[48px] leading-[48px] tracking-normal"
@@ -137,7 +260,7 @@ document.addEventListener("DOMContentLoaded", () => {
             </p>
 
             <div class="flex justify-center md:w-[475px] md:h-[520px] items-center gap-3 md:gap-10">
-              <div id="arrow-right" class="size-6 md:size-10 cursor-pointer">
+              <div id="arrow-right" class="size-6  md:size-10 cursor-pointer">
                 <img
                   class="w-full  "
                   src="./images/next-slider.svg"
@@ -245,15 +368,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const arrowRight = document.querySelector("#arrow-right");
     const arrowLeft = document.querySelector("#arrow-left");
 
-    
-
-    
-
-        arrowRight.addEventListener("click", (e) => {
+    arrowRight.addEventListener("click", (e) => {
       e.preventDefault();
       SliderIndexData =
         SliderIndexData < SliderData.length - 1 ? SliderIndexData + 1 : 0;
-                console.log('sl',SliderIndexData);
+      console.log("sl", SliderIndexData);
 
       changeSliderData(SliderIndexData);
     });
@@ -262,19 +381,20 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault();
       SliderIndexData =
         SliderIndexData === 0 ? SliderData.length - 1 : SliderIndexData - 1;
-        console.log('sl',SliderIndexData);
-        
+      console.log("sl", SliderIndexData);
+
       changeSliderData(SliderIndexData);
     });
   }
   changeSliderData(SliderIndexData);
 });
 
-
 document.querySelectorAll(".des h3").forEach((heading) => {
   heading.addEventListener("click", function () {
     const allParagraphs = document.querySelectorAll(".des p");
     const nextParagraph = this.nextElementSibling;
+
+    
 
     allParagraphs.forEach((p) => {
       if (p !== nextParagraph) {
